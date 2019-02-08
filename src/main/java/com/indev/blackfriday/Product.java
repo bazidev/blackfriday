7303d39e18064c9964272fd59a4471b9511fa5dc
melbazi <melbazi@sqli.com>
2019-02-08 12:05:44 +0100

    valid all tests

--- src/main/java/com/indev/blackfriday/Product.java
+++ src/main/java/com/indev/blackfriday/Product.java
@@ -5,23 +5,51 @@ public class Product {
     private int quantity;
     private float price;
     private float asserts;
+    private boolean isBlackFriday;
 
     public Product(int quantity, float price) {
         this.quantity = quantity;
         this.price = price;
-        this.asserts = quantity*price;
+        this.asserts = quantity * price;
     }
 
-    public float countAsserts(){
+    public float countAsserts() {
         return asserts;
     }
 
-    public float sells(){
-        updateAssetrs();
-        return (float) (5*price+5*price*0.2);
+    public float sells(boolean isBlackFriday) {
+        if (isBlackFriday) {
+            updateBlackFridayQuantity();
+            updateBlackFridayAssetrs();
+            return (float) (11 * price );
+        }
+        else{
+            updateQuantity();
+            updateAssetrs();
+            return (float) (6 * price);
+        }
+    }
+
+    private void updateBlackFridayQuantity() {
+        if (quantity < 10) {
+            throw new RuntimeException(quantity + " is not enough");
+        }
+        quantity = quantity - 10;
+
+    }
+
+    private void updateBlackFridayAssetrs() {
+        asserts = (float) (asserts + 10 * price * 0.1);
+    }
+
+    private void updateQuantity() {
+        if (quantity < 5) {
+            throw new RuntimeException(quantity + " is not enough");
+        }
+        quantity = quantity - 5;
     }
 
     private void updateAssetrs() {
-        asserts = (float) (asserts + 5*price*0.2);
+        asserts = (float) (asserts + 5 * price * 0.2);
     }
 }
